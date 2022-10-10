c9044f3dd4e3e269a0fdb45c895e989663330502
Piippo Juuso Heikki Aleksanteri <juuso.piippo1@gmail.com>
2022-10-10 15:51:54 +0300

    Tehty kenttä ja yksi aliohjelma

--- peli/AlienEscape/AlienEscape.cs
+++ peli/AlienEscape/AlienEscape.cs
@@ -9,30 +9,43 @@ namespace AlienEscape
 {
     public class AlienEscape : PhysicsGame
     {
-        private static readonly String[] kentta = {
+        private static readonly String[] kenttamj = {
+
+            "XXXXXXXXXXXXXXXXXXXX",
+            "XXXXXXXXXXXXXXXXXXXX",
+            "XXXXXXXXXXXXXXX   EX",
+            "XXXXXXX   b     XXXX",
+            "X   V    XXXXXXXXXXX",
+            "X T     H      XXXXX",
+            "XXXXXXXXXXXXXX     X",
+            "XXXXX        XXXX  X",
+            "X    BXXX  X X    XX",
+            "X   XXX   XX     XXX",
+            "X12 D   XXXXXXX XXXX",
+            "===============A====",
 
         };
 
-        private static readonly int tileWidth = 800 / kentta[0].Length;
-        private static readonly int tileHeight = 480 / kentta.Length;
+        private static readonly int tileWidth = 800 / (kenttamj[0].Length);
+        private static readonly int tileHeight = 480 / kenttamj.Length;
         public override void Begin()
         {
             Level.Background.CreateGradient(Color.Blue, Color.White);
-            TileMap kentta = TileMap.FromStringArray(kentta);
+            TileMap kentta = TileMap.FromStringArray(kenttamj);
 
-            kentta.SetTileMethod('X', LuoSeina);
-            kentta.SetTileMethod('=', LuoLattia);
-            kentta.SetTileMethod('A', LuoPiikki);
-            kentta.SetTileMethod('V', LuoLaser);
-            kentta.SetTileMethod('T', LuoAarre);
-            kentta.SetTileMethod('D', LuoOvi);
-            kentta.SetTileMethod('B', LuoPainike);
-            kentta.SetTileMethod('b', LuoPainike2);
-            kentta.SetTileMethod('H', LuoHissi);
-            kentta.SetTileMethod('P', LuoPelaaja1);
-            kentta.SetTileMethod('p', LuoPelaaja2);
-            kentta.SetTileMethod('*', LuoVihollinen);
-            kentta.SetTileMethod('E', LuoExit);
+            kentta.SetTileMethod('X', LuoPalikka);
+            kentta.SetTileMethod('=', LuoPalikka);
+            // TODO: kentta.SetTileMethod('A', LuoPiikki);
+            // TODO: kentta.SetTileMethod('V', LuoLaser);
+            // TODO: kentta.SetTileMethod('T', LuoAarre);
+            // TODO: kentta.SetTileMethod('D', LuoOvi);
+            // TODO: kentta.SetTileMethod('B', LuoPainike);
+            // TODO: kentta.SetTileMethod('b', LuoPainike2);
+            // TODO: kentta.SetTileMethod('H', LuoHissi);
+            // TODO: kentta.SetTileMethod('1', LuoPelaaja1);
+            // TODO: kentta.SetTileMethod('2', LuoPelaaja2);
+            // TODO: kentta.SetTileMethod('*', LuoVihollinen);
+            // TODO: kentta.SetTileMethod('E', LuoExit);
 
             kentta.Execute(tileWidth, tileHeight); // Luodaan kenttä
 
@@ -41,6 +54,14 @@ namespace AlienEscape
 
             PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
             Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+        }
+
+        private void LuoPalikka(Vector paikka, double leveys, double korkeus)
+        {
+            PhysicsObject palikka = new PhysicsObject(leveys-3, korkeus-3);
+            palikka.Position = paikka;
+            //TODO: palikka.Image = 
+            Add(palikka);
         }
     }
 }
