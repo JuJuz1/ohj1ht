526e5d2acea08d5fc7370cdefdac7e9113dcff63
Piippo Juuso Heikki Aleksanteri <juuso.piippo1@gmail.com>
2022-10-10 14:48:41 +0300

    Alustavat koodit

--- peli/AlienEscape/AlienEscape.cs
+++ peli/AlienEscape/AlienEscape.cs
@@ -9,9 +9,35 @@ namespace AlienEscape
 {
     public class AlienEscape : PhysicsGame
     {
+        private static readonly String[] kentta = {
+
+        };
+
+        private static readonly int tileWidth = 800 / kentta[0].Length;
+        private static readonly int tileHeight = 480 / kentta.Length;
         public override void Begin()
         {
-            // Kirjoita ohjelmakoodisi tähän
+            Level.Background.CreateGradient(Color.Blue, Color.White);
+            TileMap kentta = TileMap.FromStringArray(kentta);
+
+            kentta.SetTileMethod('X', LuoSeina);
+            kentta.SetTileMethod('=', LuoLattia);
+            kentta.SetTileMethod('A', LuoPiikki);
+            kentta.SetTileMethod('V', LuoLaser);
+            kentta.SetTileMethod('T', LuoAarre);
+            kentta.SetTileMethod('D', LuoOvi);
+            kentta.SetTileMethod('B', LuoPainike);
+            kentta.SetTileMethod('b', LuoPainike2);
+            kentta.SetTileMethod('H', LuoHissi);
+            kentta.SetTileMethod('P', LuoPelaaja1);
+            kentta.SetTileMethod('p', LuoPelaaja2);
+            kentta.SetTileMethod('*', LuoVihollinen);
+            kentta.SetTileMethod('E', LuoExit);
+
+            kentta.Execute(tileWidth, tileHeight); // Luodaan kenttä
+
+            Level.CreateBorders();
+            Camera.ZoomToLevel();
 
             PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
             Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
